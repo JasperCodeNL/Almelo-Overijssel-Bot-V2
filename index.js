9bc9b6db4ffc12f0e219532eb4e284f7d08c495e
Jasper_K <Jasper.Kriege@outlook.com>
2021-09-16 19:57:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const discord = require("discord.js");
-const botConfig = require("./botConfig.json");
-
 const fs = require("fs");
+const botConfig = require("./botConfig.json");
 
 const client = new discord.Client();
 client.commands = new discord.Collection();
