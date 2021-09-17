691f19c934d8f3a84f300e0e17708ccb421c68f7
Jasper_K <Jasper.Kriege@outlook.com>
2021-09-17 20:24:31 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async(bot, message, args) => {
     .setTimestamp()
     .setColor("BLUE")
     .addFields(
-        {name:"Standaart commands:", value:"?help - Alle commands. \n ?hallo - Bot zegt hallo terug.  \n ?test - Kijk of de bot werkt. \n ?serverinfo - Krijg info over deze server."},
+        {name:"Standaart commands:", value:"?help - Alle commands. \n ?hallo - Bot zegt hallo terug.  \n ?test - Kijk of de bot werkt. \n ?members - Zie hoeveel mensen in de discord zijn. \n ?serverinfo - Krijg info over deze server. \n ?suggestie - Laat een suggestie achter voor de game."},
         {name:"Moderatie commands:", value:"?kick - Kick een speler. \n ?ban - Ban een speler. \n ?clear (Getal) - Verwijder berichten. \n ?close - Sluit een ticket. \n ?add - Voeg iemand toe aan je ticket. \n ?remove - Verwijder iemand uit je ticket."}
     )
     
