1ded59bec62725383beba8b3eca5d10329723266
Jasper_K <Jasper.Kriege@outlook.com>
2021-09-18 12:10:59 +0200

    init

--- commands/discordupdate.js
+++ commands/discordupdate.js
@@ -21,7 +21,7 @@ module.exports.run = async(bot, message, args) => {
         .setColor("GREEN")
         .setTimestamp()
         .setFooter("Updates")
-        .setDescription("Update succesvlo geplaatst!");
+        .setDescription("**Update succesvlo geplaatst!**");
         
 
     var argsList = args.join(" ").split(seperator);
