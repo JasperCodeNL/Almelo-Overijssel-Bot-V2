f624828404cf76b72dbabc28cf368fdedc560d5a
Jasper_K <Jasper.Kriege@outlook.com>
2021-09-22 08:41:44 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -3,8 +3,23 @@ module.exports.run = async(bot, message, args) => {
     const categoryID = "873968786968035329";
 
     if(message.channel.parentID == categoryID){
+
+        var LogEmbed =  new discord.MessageEmbed()
+        .setTitle("Ticket Gesloten")
+        .setFooter("Logs")
+        .setTimestamp()
+        .setColor("BLUE")
+        .addFields(
+            { name: "Gebruiker:", value: `${message.author.tag} (${message.author.id})` },
+            { name: "Ticket:", value: `${message.channel.name}` }
+        );
+    
+        bot.channels.cache.get('889811265738919977').send(LogEmbed);
+        
         message.channel.delete();
     }
+
+
 
 }
 
