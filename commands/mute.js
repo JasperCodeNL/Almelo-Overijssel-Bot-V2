26ac9b1d3bd9d3870ffc87e7b5de320b78af1feb
Jasper_K <Jasper.Kriege@outlook.com>
2021-09-22 09:04:15 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -2,8 +2,8 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
     var botEmbed = new discord.MessageEmbed()
-    .setDescription("**Ik werk!**")
-    .setColor("GREEN") 
+    .setDescription("**Command nog niet beschikbaar!**")
+    .setColor("BLUE") 
     
 return message.channel.send(botEmbed);
 
