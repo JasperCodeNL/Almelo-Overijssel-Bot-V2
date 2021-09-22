e9ffa747b374efc2e9c411d98c3e1a45ec3ab5c0
Jasper_K <Jasper.Kriege@outlook.com>
2021-09-22 08:28:01 +0200

    Update new.js

--- commands/new.js
+++ commands/new.js
@@ -66,7 +66,7 @@ module.exports.run = async(bot, message, args) => {
                     .setTimestamp()
                     .setColor("BLUE")
                     .addFields(
-                        { name: "Gebruiker:", value: `${message.author.tag} (${newMessage.author.id})` },
+                        { name: "Gebruiker:", value: `${message.author.tag} (${message.author.id})` },
                         { name: "Ticket:", value: `${message.author.tag}` }
                     );
 
