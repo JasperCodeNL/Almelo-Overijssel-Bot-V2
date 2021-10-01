e0e0fcb92acf1aa6d4e6a039d863d1712f55f2ad
Jasper_K <Jasper.Kriege@outlook.com>
2021-10-01 19:21:46 +0200

    Create giveaway.js

---
+++ commands/giveaway.js
@@ -0,0 +1,93 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    var item = "";
+    var time = "";
+    var winnerCount = "";
+
+    if(!message.member.hasPermission("BAN_MEMBERS")) return;
+
+    winnerCount = args[0];
+    time = args[1];
+    item = args.splice(2, args.length).join(" ");
+
+    if(!winnerCount) return;
+    if(!time) return;
+    if(!item) return;
+
+    message.delete();
+
+    var date = new Date().getTime();
+    var dateEnd = new Date(date + (time * 1000));
+
+    var giveawayEmbed = new discord.MessageEmbed()
+    .setTitle("🎉GiveAway!🎉")
+    .setFooter(`Eindigt: ${dateEnd}!`)
+    .setColor("BLUE")
+    .setDescription(`Prijs: \n ${item}`);
+
+    var embedSend = await message.channel.send(giveawayEmbed);
+    embedSend.react("🎉");
+
+    setTimeout(function(){
+
+        var random = 0;
+        var winners = [];
+        var inList = false;
+
+        var peopleReacted = embedSend.reactions.cache.get("🎉").users.cache.array();
+
+        for (let i = 0; i < peopleReacted.length; i++) {
+            
+            if(peopleReacted[i].id == bot.user.id){
+                peopleReacted.splice(i,1);
+                continue;
+            }
+            
+        }
+
+        if (peopleReacted.length == 0){
+            return message.channel.send("**Niemand heeft gewonnen!!!**");
+        }
+
+        if (peopleReacted.length < winnerCount) {
+            return message.channel.send("**Niemand deet mee!!!**");
+        }
+
+        for (let y = 0; y < winnerCount; y++) {
+            
+            inList = false;
+
+            random = Math.floor(Math.random() * peopleReacted.length);
+
+            for (let o = 0; o < winners.length; o++) {
+
+                if(winners[o] == peopleReacted[random]){
+                    inList = true;
+                    y--;
+                    break;
+                }                
+                
+            }
+            
+            if(!inList){
+                winners.push(peopleReacted[random]);
+            }
+
+        }
+
+        for (let y = 0; y < winners.length; y++) {
+
+            message.channel.send("Gefeliciteerd **" + winners[y].username + `** Je hebt gewonnen **${item}**`);
+            
+        }
+
+
+    }, time * 1000)
+
+}
+
+module.exports.help = {
+    name: "giveaway"
+}
