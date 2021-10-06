c3c3bf3f62086befeb4084a364d9dc7c3edcbe1b
Jasper_K <Jasper.Kriege@outlook.com>
2021-10-06 19:12:36 +0200

    Create add copy.js

---
+++ commands/add copy.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    var categoryID = "873968786968035329";
+
+    if(!message.channel.parentID == categoryID) return message.reply("Haha wat grappig!");
+
+    var LogEmbed =  new discord.MessageEmbed()
+    .setTitle("Ticket Gesloten")
+    .setFooter("Logs")
+    .setTimestamp()
+    .setColor("BLUE")
+    .addFields(
+        { name: "Gesloten Door:", value: `${message.author.tag} (${message.author.id})` },
+        { name: "Ticket:", value: `${message.channel.name}` }
+    );
+
+    bot.channels.cache.get('889811265738919977').send(LogEmbed);
+
+    message.channel.delete();
+
+  
+}
+
+
+module.exports.help = {
+    name: "close"
+}
