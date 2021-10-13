1efbabba5adf60ef5ed1040a5a79b007bb85d526
Jasper_K <Jasper.Kriege@outlook.com>
2021-10-13 15:48:44 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -6,21 +6,29 @@ module.exports.run = async(bot, message, args) => {
 
     if(!message.member.roles.cache.has('793838521826672660')) return message.reply("Je hebt geen perms!");
 
-    if(!message.channel.parentID == categoryID) return message.reply("Haha wat grappig!");
+    if(message.channel.parentID == categoryID) {
 
-    var LogEmbed =  new discord.MessageEmbed()
-    .setTitle("Ticket Gesloten")
-    .setFooter("Logs")
-    .setTimestamp()
-    .setColor("BLUE")
-    .addFields(
-        { name: "Gesloten Door:", value: `${message.author.tag} (${message.author.id})` },
-        { name: "Ticket:", value: `${message.channel.name}` }
-    );
+        var LogEmbed =  new discord.MessageEmbed()
+        .setTitle("Ticket Gesloten")
+        .setFooter("Logs")
+        .setTimestamp()
+        .setColor("BLUE")
+        .addFields(
+            { name: "Gesloten Door:", value: `${message.author.tag} (${message.author.id})` },
+            { name: "Ticket:", value: `${message.channel.name}` }
+        );
+    
+        bot.channels.cache.get('889811265738919977').send(LogEmbed);
+    
+        message.channel.delete();
 
-    bot.channels.cache.get('889811265738919977').send(LogEmbed);
+    } else {
 
-    message.channel.delete();
+        return message.reply("Haha wat grappig!");
+
+    }
+
+   
 
   
 }
