e655cc5004b1082c277973f8c8b5eb2bf771ee5c
Jasper_K <Jasper.Kriege@outlook.com>
2021-10-13 08:17:08 +0200

    Create rename.js

---
+++ commands/rename.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    var categoryID = "873968786968035329";
+
+    if(!message.member.roles.cache.has('793838521826672660')) return message.reply("Je hebt geen perms!");
+
+    if(!message.channel.parentID == categoryID) return message.reply("Haha wat grappig!");
+
+    var newNaam = args[0]
+
+    message.channel.name = newNaam
+
+}
+
+
+module.exports.help = {
+    name: "remove"
+}
